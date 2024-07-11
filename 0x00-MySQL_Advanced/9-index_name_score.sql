3852f214e1def3baea2d8ac6bef910038a18b997
BizNextStep <121868497+BizNextStep@users.noreply.github.com>
2024-07-11 21:21:21 +0200

    Update 9-index_name_score.sql

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -1,11 +1,6 @@
--- SQL script to create an index idx_name_first_score on the first letter of name and score
+-- Creates an index idx_name_first_score on the names table
+-- The index is on the first letter of the name column and the score column
+-- Only the first letter of the name and the score are indexed
 
--- Drop index if exists (in case script needs to be rerun)
-DROP INDEX IF EXISTS idx_name_first_score ON names;
-
--- Create index on the first letter of the name column and score
-CREATE INDEX idx_name_first_score ON names (LEFT(name, 1), score);
-
--- Show index information after creation
-SHOW INDEX FROM names;
-
+CREATE INDEX idx_name_first_score
+ON names (name(1), score);
