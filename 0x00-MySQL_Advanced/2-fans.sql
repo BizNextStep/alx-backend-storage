1409077650f3959117d5f9e848b996fda9346656
BizNextStep <121868497+BizNextStep@users.noreply.github.com>
2024-07-11 21:07:42 +0200

    Update 2-fans.sql

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,8 +1,9 @@
-SELECT origin, COUNT(*) AS total_fans
-FROM (
-  SELECT origin, COUNT(*) AS nb_fans
-  FROM metal_bands
-  GROUP BY origin
-) AS subquery
+-- Write SQL script that ranks country origins of bands
+-- Ordered by the number of (non-unique) fans
+-- Column must be origin and nb_fans
+-- Script can be executed on any database
+
+SELECT origin, SUM(fans) AS nb_fans
+FROM metal_bands
 GROUP BY origin
-ORDER BY total_fans DESC;
+ORDER BY nb_fans DESC;
