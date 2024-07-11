c7cffbe4839faed95d116552469eb38218fc3dd5
BizNextStep <121868497+BizNextStep@users.noreply.github.com>
2024-07-11 21:11:40 +0200

    Update 7-average_score.sql

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,32 +1,15 @@
--- SQL script to create a stored procedure ComputeAverageScoreForUser
-DELIMITER //
+-- Create stored procedure ComputeAverageScoreForUser
+-- Computes and stores average score for a student
+-- Takes 1 input parameter: user_id (users.id)
 
-CREATE PROCEDURE ComputeAverageScoreForUser(
-    IN user_id INT
-)
+DELIMITER $$
+
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
 BEGIN
-    DECLARE total_score FLOAT;
-    DECLARE num_corrections INT;
-    DECLARE avg_score FLOAT;
-    
-    -- Calculate total score and number of corrections for the user
-    SELECT SUM(score), COUNT(*) INTO total_score, num_corrections
-    FROM corrections
-    WHERE user_id = user_id;
-    
-    -- Compute average score
-    IF num_corrections > 0 THEN
-        SET avg_score = total_score / num_corrections;
-    ELSE
-        SET avg_score = 0; -- default value if no corrections found
-    END IF;
-    
-    -- Update the user's average_score in the users table
     UPDATE users
-    SET average_score = avg_score
+    SET average_score = (SELECT AVG(score) FROM corrections WHERE corrections.user_id = user_id)
     WHERE id = user_id;
-    
-END //
+END $$
 
 DELIMITER ;
-
