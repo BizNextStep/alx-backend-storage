20d946b516626d6af6441338ca670da0bee33b0c
BizNextStep <121868497+BizNextStep@users.noreply.github.com>
2024-07-16 22:11:04 +0200

    Update 10-update_topics.py

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,32 +1,16 @@
 #!/usr/bin/env python3
 """
-This module provides a function to update topics of a school document based on the name.
+Changes all topics of a schooll document based on the name
+Prototype: def update_topics(mongo_collection, name, topics):
+mongo_collection will be the pymongo object
+name (String) will be the school name to update
+topics (list of strings) will be the list of topics approached in the school
 """
+
 
 def update_topics(mongo_collection, name, topics):
     """
-    Changes all topics of a school document based on the name.
+    Prototype: def update_topics(mongo_collection, name, topics):
+    Changes all topics of a school document
     """
-    mongo_collection.update_many(
-        { "name": name },
-        { "$set": { "topics": topics } }
-    )
-
-if __name__ == "__main__":
-    from pymongo import MongoClient
-
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    school_collection = client.my_db.school
-    update_topics(school_collection, "Holberton school", ["Sys admin", "AI", "Algorithm"])
-
-    from 8-all import list_all
-    schools = list_all(school_collection)
-    for school in schools:
-        print("[{}] {} {}".format(school.get('_id'), school.get('name'), school.get('topics', "")))
-
-    update_topics(school_collection, "Holberton school", ["iOS"])
-
-    schools = list_all(school_collection)
-    for school in schools:
-        print("[{}] {} {}".format(school.get('_id'), school.get('name'), school.get('topics', "")))
-
+    mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
